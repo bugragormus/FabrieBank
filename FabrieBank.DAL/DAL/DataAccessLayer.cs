547ddd4150b8d75365b18afee23ec8c0c049d837
Buğra Görmüş <95827988+bugragormus@users.noreply.github.com>
2023-07-25 15:36:36 +0300

    Update DataAccessLayer.cs

--- FabrieBank.DAL/DAL/DataAccessLayer.cs
+++ FabrieBank.DAL/DAL/DataAccessLayer.cs
@@ -95,12 +95,6 @@ namespace FabrieBank.DAL
                     {
                         command.Parameters.AddWithValue("@musteri_id", musteriId);
 
-                        // Define OUT parameters to retrieve the result data
-                        command.Parameters.Add(new NpgsqlParameter("hesap_no", NpgsqlDbType.Bigint) { Direction = ParameterDirection.Output });
-                        command.Parameters.Add(new NpgsqlParameter("bakiye", NpgsqlDbType.Numeric) { Direction = ParameterDirection.Output });
-                        command.Parameters.Add(new NpgsqlParameter("doviz_cins", NpgsqlDbType.Integer) { Direction = ParameterDirection.Output });
-                        command.Parameters.Add(new NpgsqlParameter("hesap_adi", NpgsqlDbType.Text) { Direction = ParameterDirection.Output });
-
                         NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(command);
                         DataTable dataTable = new DataTable();
 
@@ -140,27 +134,32 @@ namespace FabrieBank.DAL
                 {
                     connection.Open();
 
-                    // Check account balance
-                    string sqlSelectBakiye = "SELECT Bakiye FROM public.Hesap WHERE HesapNo = @hesapNo";
+                    string functionName = "func_GetBakiye";
+
+                    string sqlSelectBakiye = $"SELECT * FROM {functionName}(@hesapNo)";
                     using (NpgsqlCommand commandSelectBakiye = new NpgsqlCommand(sqlSelectBakiye, connection))
                     {
                         commandSelectBakiye.Parameters.AddWithValue("@hesapNo", hesapNo);
 
-                        object result = commandSelectBakiye.ExecuteScalar();
-                        if (result == null)
+                        NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(commandSelectBakiye);
+                        DataTable dataTable = new DataTable();
+
+                        npgsqlDataAdapter.Fill(dataTable);
+
+                        //object result = commandSelectBakiye.ExecuteScalar();
+                        if (dataTable.Rows.Count == null)
                         {
                             Console.WriteLine("\nHesap bulunamadı.");
                             return false;
                         }
 
-                        decimal bakiye = Convert.ToDecimal(result);
+                        decimal bakiye = Convert.ToDecimal(dataTable.Rows[0][0]);
                         if (bakiye != 0)
                         {
                             Console.WriteLine("\nHesap bakiyesi 0 değil. Lütfen bakiyeyi başka bir hesaba aktarın.");
                             return false;
                         }
                     }
-
                     // Delete the account
                     string sqlDeleteHesap = "DELETE FROM public.Hesap WHERE HesapNo = @hesapNo";
                     using (NpgsqlCommand commandDeleteHesap = new NpgsqlCommand(sqlDeleteHesap, connection))
@@ -181,6 +180,7 @@ namespace FabrieBank.DAL
                     }
                 }
             }
+
             catch (Exception ex)
             {
                 // Log the error to the database using the ErrorLoggerDB
@@ -1299,77 +1299,3 @@ namespace FabrieBank.DAL
         }
     }
 }
-
-
-//public bool DeleteAccount(long hesapNo)
-//{
-//    try
-//    {
-//        using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
-//        {
-//            connection.Open();
-
-//            string procedureName = "usp_GetBakiye";
-
-//            string sqlSelectBakiye = $"CALL {procedureName}(@hesapNo)";
-//            using (NpgsqlCommand commandSelectBakiye = new NpgsqlCommand(sqlSelectBakiye, connection))
-//            {
-//                commandSelectBakiye.Parameters.AddWithValue("@hesapNo", hesapNo);
-
-//                commandSelectBakiye.Parameters.Add(new NpgsqlParameter("hesapNo", NpgsqlDbType.Bigint) { Direction = ParameterDirection.Output });
-
-//                NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(commandSelectBakiye);
-//                DataTable dataTable = new DataTable();
-
-//                npgsqlDataAdapter.Fill(dataTable);
-
-//                foreach (DataRow item in dataTable.Rows)
-//                {
-//                    //object result = commandSelectBakiye.ExecuteScalar();
-//                    if (dataTable == null)
-//                    {
-//                        Console.WriteLine("\nHesap bulunamadı.");
-//                        return false;
-//                    }
-
-//                    decimal bakiye = Convert.ToDecimal(dataTable);
-//                    if (bakiye != 0)
-//                    {
-//                        Console.WriteLine("\nHesap bakiyesi 0 değil. Lütfen bakiyeyi başka bir hesaba aktarın.");
-//                        return false;
-//                    }
-//                }
-
-//            }
-
-//            // Delete the account
-//            string sqlDeleteHesap = "DELETE FROM public.Hesap WHERE HesapNo = @hesapNo";
-//            using (NpgsqlCommand commandDeleteHesap = new NpgsqlCommand(sqlDeleteHesap, connection))
-//            {
-//                commandDeleteHesap.Parameters.AddWithValue("@hesapNo", hesapNo);
-
-//                int affectedRows = commandDeleteHesap.ExecuteNonQuery();
-//                if (affectedRows > 0)
-//                {
-//                    Console.WriteLine("\nHesap başarıyla silindi.");
-//                    return true;
-//                }
-//                else
-//                {
-//                    Console.WriteLine("\nHesap silinemedi. Lütfen tekrar deneyin.");
-//                    return false;
-//                }
-//            }
-//        }
-//    }
-//    catch (Exception ex)
-//    {
-//        // Log the error to the database using the ErrorLoggerDB
-//        MethodBase method = MethodBase.GetCurrentMethod();
-//        LogError(ex, method.ToString());
-
-//        // Handle the error (display a user-friendly message, rollback transactions, etc.)
-//        Console.WriteLine($"An error occurred while performing {method} operation. Please try again later.");
-//        return false;
-//    }
-//}
