f86056b59356668d45fc3a178abf2a9e1ff0d7fc
Buğra Görmüş <95827988+bugragormus@users.noreply.github.com>
2023-07-26 14:41:31 +0300

    New Era

--- FabrieBank.BLL/Logic/AccountLogic.cs
+++ FabrieBank.BLL/Logic/AccountLogic.cs
@@ -1,6 +1,9 @@
-﻿using FabrieBank.Common;
+﻿using System.Reflection;
+using FabrieBank.Common;
+using FabrieBank.Common.DTOs;
 using FabrieBank.Common.Enums;
 using FabrieBank.Entity;
+using Npgsql;
 
 namespace FabrieBank.BLL
 {
@@ -58,6 +61,174 @@ namespace FabrieBank.BLL
             Console.WriteLine("\nSilmek istediğiniz hesap numarasını girin: ");
             long hesapNo = long.Parse(Console.ReadLine());
             _ = accInfoDB.HesapSil(hesapNo);
+        }
+
+        public void Deposit(long hesapNo, decimal bakiye)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
+                {
+                    connection.Open();
+
+                    string functionName = "func_ReadAccountInfo";
+
+                    string sqlSelect = $"SELECT * FROM {functionName}(@hesapNo)";
+                    string sqlUpdate = "UPDATE public.Hesap SET Bakiye = Bakiye + @bakiye WHERE HesapNo = @hesapNo";
+
+                    using (NpgsqlCommand commandSelect = new NpgsqlCommand(sqlSelect, connection))
+                    {
+                        commandSelect.Parameters.AddWithValue("@hesapNo", hesapNo);
+
+                        decimal eskiBakiye = Convert.ToDecimal(commandSelect.ExecuteScalar());
+                        decimal yeniBakiye = eskiBakiye + bakiye;
+
+                        using (NpgsqlCommand commandUpdate = new NpgsqlCommand(sqlUpdate, connection))
+                        {
+                            commandUpdate.Parameters.AddWithValue("@bakiye", bakiye);
+                            commandUpdate.Parameters.AddWithValue("@hesapNo", hesapNo);
+
+                            int rowsAffected = commandUpdate.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+
+                                // Log the successful deposit
+                                DTOTransactionLog transactionLog = new DTOTransactionLog
+                                {
+                                    AccountNumber = hesapNo,
+                                    TransactionType = EnumTransactionType.Deposit,
+                                    TransactionStatus = EnumTransactionStatus.Success,
+                                    Amount = yeniBakiye - eskiBakiye,
+                                    OldBalance = eskiBakiye,
+                                    NewBalance = yeniBakiye,
+                                    Timestamp = DateTime.Now
+                                };
+
+                                LogTransaction(transactionLog);
+
+                                Console.WriteLine("\nPara yatırma işlemi başarılı.");
+                                Console.WriteLine($"Eski bakiye: {eskiBakiye}");
+                                Console.WriteLine($"Yeni bakiye: {yeniBakiye}");
+                            }
+                            else
+                            {
+
+                                // Log the failed deposit
+                                DTOTransactionLog transactionLog = new DTOTransactionLog
+                                {
+                                    AccountNumber = hesapNo,
+                                    TransactionType = EnumTransactionType.Deposit,
+                                    TransactionStatus = EnumTransactionStatus.Failed,
+                                    Amount = yeniBakiye - eskiBakiye,
+                                    OldBalance = eskiBakiye,
+                                    NewBalance = yeniBakiye,
+                                    Timestamp = DateTime.Now
+                                };
+
+                                LogTransaction(transactionLog);
+
+                                Console.WriteLine("\nPara yatırma işlemi başarısız.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the database using the ErrorLoggerDB
+                MethodBase method = MethodBase.GetCurrentMethod();
+                LogError(ex, method.ToString());
+
+                // Handle the error (display a user-friendly message, rollback transactions, etc.)
+                Console.WriteLine("An error occurred while performing ParaYatirma operation. Please try again later.");
+            }
+        }
+
+        public void Withdraw(long hesapNo, decimal bakiye)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
+                {
+                    connection.Open();
+
+                    string sqlSelect = "SELECT Bakiye FROM public.Hesap WHERE HesapNo = @hesapNo";
+                    string sqlUpdate = "UPDATE public.Hesap SET Bakiye = @bakiye WHERE HesapNo = @hesapNo";
+
+                    using (NpgsqlCommand commandSelect = new NpgsqlCommand(sqlSelect, connection))
+                    {
+                        commandSelect.Parameters.AddWithValue("@hesapNo", hesapNo);
+
+                        decimal eskiBakiye = Convert.ToDecimal(commandSelect.ExecuteScalar());
+                        decimal yeniBakiye = eskiBakiye - bakiye;
+
+                        if (yeniBakiye >= 0)
+                        {
+                            using (NpgsqlCommand commandUpdate = new NpgsqlCommand(sqlUpdate, connection))
+                            {
+                                commandUpdate.Parameters.AddWithValue("@bakiye", yeniBakiye);
+                                commandUpdate.Parameters.AddWithValue("@hesapNo", hesapNo);
+
+                                int rowsAffected = commandUpdate.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    // Log the successful withdrawal
+                                    DTOTransactionLog transactionLog = new DTOTransactionLog
+                                    {
+                                        AccountNumber = hesapNo,
+                                        TransactionType = EnumTransactionType.Withdrawal,
+                                        TransactionStatus = EnumTransactionStatus.Success,
+                                        Amount = eskiBakiye - yeniBakiye,
+                                        OldBalance = eskiBakiye,
+                                        NewBalance = yeniBakiye,
+                                        Timestamp = DateTime.Now
+                                    };
+
+                                    LogTransaction(transactionLog);
+
+                                    Console.WriteLine("\nPara çekme işlemi başarılı.");
+                                    Console.WriteLine($"Eski bakiye: {eskiBakiye}");
+                                    Console.WriteLine($"Yeni bakiye: {yeniBakiye}");
+                                }
+                                else
+                                {
+
+                                    // Log the failed withdraw
+                                    DTOTransactionLog transactionLog = new DTOTransactionLog
+                                    {
+                                        AccountNumber = hesapNo,
+                                        TransactionType = EnumTransactionType.Withdrawal,
+                                        TransactionStatus = EnumTransactionStatus.Failed,
+                                        Amount = eskiBakiye - yeniBakiye,
+                                        OldBalance = eskiBakiye,
+                                        NewBalance = yeniBakiye,
+                                        Timestamp = DateTime.Now
+                                    };
+
+                                    LogTransaction(transactionLog);
+
+                                    Console.WriteLine("\nPara çekme işlemi başarısız.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\nYetersiz bakiye. Para çekme işlemi gerçekleştirilemedi.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the database using the ErrorLoggerDB
+                MethodBase method = MethodBase.GetCurrentMethod();
+                LogError(ex, method.ToString());
+
+                // Handle the error (display a user-friendly message, rollback transactions, etc.)
+                Console.WriteLine("An error occurred while performing ParaCekme operation. Please try again later.");
+            }
         }
     }
 }
---
+++ FabrieBank.DAL/Entity/EAccountInfo.cs
@@ -0,0 +1,203 @@
+﻿using System.Data;
+using System.Reflection;
+using FabrieBank.Common;
+using FabrieBank.Common.Enums;
+using FabrieBank.DAL;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace FabrieBank.Entity
+{
+    public class EAccountInfo
+    {
+        private DataAccessLayer dataAccessLayer;
+
+        public EAccountInfo()
+        {
+            dataAccessLayer = new DataAccessLayer();
+        }
+
+        public List<DTOAccountInfo> AccInfo(int musteriId)
+        {
+            return dataAccessLayer.ReadListAccountInfo(musteriId);
+        }
+
+        public bool HesapSil(long hesapNo)
+        {
+            return dataAccessLayer.DeleteAccount(hesapNo);
+        }
+
+        public bool UpdateAccountInfo(DTOAccountInfo dTOAccount)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
+                {
+                    connection.Open();
+
+                    string functionName = "usp_UpdateAccountInfo";
+
+                    string sqlQuery = $"SELECT * FROM {functionName}(@p_hesapno, @p_bakiye, @p_musteriid, @p_doviz_cins, @p_hesap_adi)";
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@p_hesapno", dTOAccount.HesapNo);
+                        command.Parameters.AddWithValue("@p_bakiye", dTOAccount.Bakiye);
+                        command.Parameters.AddWithValue("@p_musteriid", dTOAccount.MusteriId);
+                        command.Parameters.AddWithValue("@p_doviz_cins", dTOAccount.DovizCins);
+                        command.Parameters.AddWithValue("@p_hesap_adi", dTOAccount.HesapAdi);
+
+                        if (command.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the database using the ErrorLoggerDB
+                MethodBase method = MethodBase.GetCurrentMethod();
+                LogError(ex, method.ToString());
+
+                // Handle the error (display a user-friendly message, rollback transactions, etc.)
+                Console.WriteLine($"An error occurred while performing {method} operation. Please try again later.");
+            }
+            return false;
+        }
+
+        public List<DTOAccountInfo> ReadListAccountInfo(DTOAccountInfo dTOAccount)
+        {
+
+            List<DTOAccountInfo> accountsList = new List<DTOAccountInfo>();
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
+                {
+                    connection.Open();
+
+                    string functionName = "func_ReadListAccountInfo";
+
+                    string sqlQuery = $"SELECT * FROM {functionName}(@p_bakiye, @p_musteri_id, @p_doviz_cins, @p_hesap_adi)";
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@p_bakiye", dTOAccount.Bakiye);
+                        command.Parameters.AddWithValue("@p_musteri_id", dTOAccount.MusteriId);
+                        command.Parameters.AddWithValue("@p_doviz_cins", dTOAccount.DovizCins);
+                        command.Parameters.AddWithValue("@p_hesap_adi", dTOAccount.HesapAdi);
+
+
+                        NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+
+                        npgsqlDataAdapter.Fill(dataTable);
+                        foreach (DataRow item in dataTable.Rows)
+                        {
+                            DTOAccountInfo dTOAccountInfo = new DTOAccountInfo
+                            {
+                                HesapNo = (long)item["hesap_no"],
+                                Bakiye = (decimal)item["bakiye"],
+                                DovizCins = (EnumDovizCinsleri.DovizCinsleri)item["doviz_cins"],
+                                HesapAdi = item["hesap_adi"].ToString(),
+                            };
+                            accountsList.Add(dTOAccountInfo);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the database using the ErrorLoggerDB
+                MethodBase method = MethodBase.GetCurrentMethod();
+                LogError(ex, method.ToString());
+
+                // Handle the error (display a user-friendly message, rollback transactions, etc.)
+                Console.WriteLine($"An error occurred while performing {method} operation. Please try again later.");
+            }
+
+            return accountInfos;
+        }
+
+        public bool DeleteAccount(long hesapNo)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(database.ConnectionString))
+                {
+                    connection.Open();
+
+                    string functionName = "func_ReadAccountInfo";
+
+                    string sqlSelectBakiye = $"SELECT * FROM {functionName}(@hesapNo)";
+
+                    using (NpgsqlCommand commandSelectBakiye = new NpgsqlCommand(sqlSelectBakiye, connection))
+                    {
+                        commandSelectBakiye.Parameters.AddWithValue("@hesapNo", hesapNo);
+
+                        NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(commandSelectBakiye);
+                        DataTable dataTable = new DataTable();
+
+                        npgsqlDataAdapter.Fill(dataTable);
+
+                        //object result = commandSelectBakiye.ExecuteScalar();
+                        if (dataTable.Rows.Count == 0)
+                        {
+                            Console.WriteLine("\nHesap bulunamadı.");
+                            return false;
+                        }
+
+                        decimal bakiye = Convert.ToDecimal(dataTable.Rows[0][0]);
+                        if (bakiye != 0)
+                        {
+                            Console.WriteLine("\nHesap bakiyesi 0 değil. Lütfen bakiyeyi başka bir hesaba aktarın.");
+                            return false;
+                        }
+                    }
+
+                    // Delete the account usp_DelHesap
+                    string procedureName = "usp_DeleteAccountInfo";
+
+                    using (NpgsqlCommand commandDeleteHesap = new NpgsqlCommand(procedureName, connection))
+                    {
+                        commandDeleteHesap.CommandType = CommandType.StoredProcedure;
+
+                        // Add the output parameter
+                        NpgsqlParameter successParam = new NpgsqlParameter("success", NpgsqlDbType.Boolean);
+                        successParam.Direction = ParameterDirection.Output;
+                        commandDeleteHesap.Parameters.Add(successParam);
+
+                        commandDeleteHesap.Parameters.AddWithValue("hesap_no", hesapNo);
+
+                        commandDeleteHesap.ExecuteNonQuery();
+
+                        // Check the output parameter to determine if the delete was successful
+                        bool success = (bool)successParam.Value;
+
+                        if (success)
+                        {
+                            Console.WriteLine("\nHesap başarıyla silindi.");
+                            return true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nHesap silinemedi. Lütfen tekrar deneyin.");
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error to the database using the ErrorLoggerDB
+                MethodBase method = MethodBase.GetCurrentMethod();
+                LogError(ex, method.ToString());
+
+                // Handle the error (display a user-friendly message, rollback transactions, etc.)
+                Console.WriteLine($"An error occurred while performing {method} operation. Please try again later.");
+                return false;
+            }
+        }
+    }
+}
